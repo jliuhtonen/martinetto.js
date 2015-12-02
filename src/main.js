344595185389400cb87584f68a80247f08f5f432
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2015-12-02 20:18:56 +0200

    Make parser main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,101 @@
-import routeParser from './parser'
+import {zip, and, flatten, intersperse} from './utils'
+import escapeStringRegexp from 'escape-string-regexp'
 
-console.log(routeParser('/foo/:bar/*')('/foo/123/abba/cabba'))
+const wildcardTokenSeparator = '*'
+const wildcardTokenSeparatorRegExp = escapeStringRegexp(wildcardTokenSeparator)
 
-export default {
-  'Parser': routeParser
+const pathTokenSeparator = '/'
+const pathTokenSeparatorRegExp = escapeStringRegexp(pathTokenSeparator)
+
+const namedParamPattern = /^:\w+$/
+
+function parseRoute(route, prefix = '') {
+  const routeToParse = pathWithoutPrefix(route, prefix)
+  const routeTokens = asTokens(routeToParse)
+  const routeRegExp = new RegExp(toPattern(routeTokens))
+  const expectedParamTokens = routeTokens
+    .filter(token => token.type !== 'literal')
+
+  return function(currentPath) {
+    const pathToMatch = pathWithoutPrefix(currentPath, prefix)
+    const paramMatches = pathToMatch.match(routeRegExp)
+    const isMatch = !!paramMatches
+
+    if(!isMatch) {
+      return null
+    } else {
+      const paramValues = paramMatches.splice(1)
+      const paramValuePairs = zip(expectedParamTokens, paramValues)
+
+      const pathParams = extractPathParams(paramValuePairs)
+      const wildcards = extractWildcardParams(paramValuePairs)
+
+      return {
+        path: currentPath,
+        pathParams,
+        wildcards
+      }
+    }
+  }
 }
+
+function pathWithoutPrefix(path, prefix) {
+  const pathHasPrefix = path.startsWith(prefix)
+  return pathHasPrefix ? path.substring(prefix.length) : path
+}
+
+function asTokens(path) {
+  const wildcardTokens = path.split('*')
+  const tokenizedPaths = wildcardTokens.map(path => {
+    return path
+    .split(pathTokenSeparator)
+    .filter(notEmpty)
+    .map(stringToPathToken)
+  })
+
+  const tokenizedWithWildcards = intersperse(tokenizedPaths,
+    { type: 'wildcard', value: 'wildcard', pattern: '([\\w\/]*)' })
+
+  return flatten(tokenizedWithWildcards)
+}
+
+function toPattern(routeTokens) {
+  return routeTokens.map(token => token.pattern).join(pathTokenSeparatorRegExp)
+}
+
+function stringToPathToken(part) {
+  if(part.match(namedParamPattern)) {
+    return {
+      type: 'pathParam',
+      value: part.substring(1),
+      pattern: '(\\w+)'
+    }
+  } else {
+    return {
+      type: 'literal',
+      value: part,
+      pattern: escapeStringRegexp(part)
+    }
+  }
+}
+
+function extractWildcardParams(paramValuePairs) {
+  return paramValuePairs
+    .filter(([param, value]) => param.type === 'wildcard')
+    .map(([param, value]) => value)
+}
+
+function extractPathParams(paramValuePairs) {
+  return paramValuePairs
+    .filter(([param, value]) => param.type === 'pathParam')
+    .reduce((params, [param, value]) => {
+      params[param.value] = value
+      return params
+    }, {})
+}
+
+function notEmpty(enumerable) {
+  return enumerable.length > 0
+}
+
+export default parseRoute
