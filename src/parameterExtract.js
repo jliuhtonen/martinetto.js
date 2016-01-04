877bbe1005a13601d7481ad8d4b08228f849e839
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-01-04 18:46:22 +0200

    Fix indentation

--- src/parameterExtract.js
+++ src/parameterExtract.js
@@ -1,14 +1,14 @@
 export function extractWildcardParams(paramValuePairs) {
   return paramValuePairs
-    .filter(([param]) => param.type === 'wildcard')
-    .map(([ , value]) => decodeURIComponent(value))
+  .filter(([param]) => param.type === 'wildcard')
+  .map(([ , value]) => decodeURIComponent(value))
 }
 
 export function extractPathParams(paramValuePairs) {
   return paramValuePairs
-    .filter(([param]) => param.type === 'pathParam')
-    .reduce((params, [param, value]) => {
-      params[param.value] = decodeURIComponent(value)
-      return params
-    }, {})
+  .filter(([param]) => param.type === 'pathParam')
+  .reduce((params, [param, value]) => {
+    params[param.value] = decodeURIComponent(value)
+    return params
+  }, {})
 }
