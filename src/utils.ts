b344747d91aafe09b98111577d77d6ba838e2bc8
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-11-20 23:16:37 +0200

    Remove unused function

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,3 @@
-export function pathWithoutPrefix(path: string, prefix: string): string {
-  const pathHasPrefix = path.indexOf(prefix) === 0
-  return pathHasPrefix ? path.substring(prefix.length) : path
-}
-
 export function findFirstTruthy<A, B>(items: Array<A>, fn: (a: A) => B): B {
   for(let i = 0; i < items.length; ++i) {
     const item = items[i]
