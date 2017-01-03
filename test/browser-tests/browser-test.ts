fb35a1d5c41696bc61d20d4ddc1e4f9224c94daa
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2017-01-03 21:25:29 +0200

    Add simple browser test cases

--- test/browser-tests/browser-test.ts
+++ test/browser-tests/browser-test.ts
@@ -1,11 +1,38 @@
 import {expect} from 'chai'
 
-describe('Refractive routing', function() {
-  it('can run router in browser', function () {
-    browser
-    .url('/test-app')
-    .pause(5000)
+const testUrlPrefix = 'http://localhost:8080/test-app'
 
-    expect(browser.getText('#app-content')).to.equal('HI')
+describe('Refractive routing', function() {
+  context('From app root', function() {
+    beforeEach(function() {
+      browser.url('/test-app')
+      .waitForExist('#app-content')
+    })
+
+    it('can run router in browser', function () {
+      expect(getAppText()).to.equal('HI')
+    })
+
+    it('runs route on clicking a link', function() {
+      browser.click('#contact-link')
+      .waitForText('#app-content')
+
+      expect(getAppText()).to.equal('Contact us')
+      expect(browser.getUrl()).to.equal(`${testUrlPrefix}/contact`)
+    })
+
+    it('runs previous route on clicking back', function() {
+      browser.click('#contact-link')
+      .waitForText('#app-content')
+      browser.back()
+
+      expect(getAppText()).to.equal('HI')
+      expect(browser.getUrl()).to.equal(`${testUrlPrefix}/`)
+    })
   })
 })
+
+function getAppText() {
+  return browser.getText('#app-content')
+}
+
